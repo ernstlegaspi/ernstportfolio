d4020bc14ff062ded601c251b6478613954a3034
ernstlegaspi <legaspiernstdanielle@gmail.com>
2023-11-03 12:12:27 +0800

    change landing page

--- app/page.js
+++ app/page.js
@@ -1,113 +1,188 @@
+'use client'
+
+import { lazy, Suspense, useState } from 'react'
+
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
+import { HiMail } from 'react-icons/hi'
 import Image from 'next/image'
 
+import me from '../img/me.jpg'
+import twitterProject from '../img/twitterclone.png'
+
+import axios from '../img/axios.png'
+import css from '../img/css.png'
+import html from '../img/html.png'
+import django from '../img/django.png'
+import express from '../img/express.png'
+import git from '../img/git.png'
+import graphql from '../img/graphql.png'
+import mongodb from '../img/mongodb.png'
+import mongoose from '../img/mongoose.png'
+import mysql from '../img/mysql.png'
+import nextjs from '../img/next.png'
+import nodejs from '../img/nodejs.png'
+import prisma from '../img/prisma.png'
+import python from '../img/python.png'
+import reactjs from '../img/reactjs.png'
+import reactquery from '../img/reactquery.png'
+import redux from '../img/redux.png'
+import tailwind from '../img/tailwind.png'
+import typescript from '../img/typescript.png'
+import zustand from '../img/zustand.webp'
+
+import AMA from '../img/ama.png'
+import dict from '../img/dict.png'
+import tup from '../img/tup.png'
+
+// const TwitterClone = lazy(() => import('./twitter-clone/twitter-clone'))
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [showTwitterClone, setShowTwitterClone] = useState(false)
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	const educationCard = (img, schoolName, schoolCourse, dateAttended) => <div className="flex flex-col items-center max-[850px]:mb-5">
+		<Image className="school-image" src={img} width={100} height={100} />
+		<p className="w-[80%] mx-auto">{schoolName}</p>
+		<p className="school-course">{schoolCourse}</p>
+		<p className="date">{dateAttended}</p>
+	</div>
+	
+	return (
+		<div className="max-width">
+			<div className="main-bg">
+				<div className="margin">
+					<div className="header">
+						<div className="header-intro">
+							<p>HEY THERE!</p>
+							<h1>I'm Ernst Legaspi</h1>
+							<p>Good day, I am Ernst Legaspi. I have 3 years personal experience and 1 year of work experience in web development using different full stack technologies, I am eager to contribute my skills to your dynamic team and assist in developing cutting-edge applications.</p>
+							<div className="flex items-center socials-color mt-1 max-[850px]:w-full max-[850px]:justify-center">
+								<a href="https://www.linkedin.com/in/er
+								nst-danielle-legaspi-259547170/" target="_blank" rel="noreferrer"><AiFillGithub size={20} /></a>
+								<a href="https://github.com/ernstlegaspi" target="_blank" rel="noreferrer"><AiFillLinkedin size={20} /></a>
+								<a href="mailto:legapsiernstdanielle@gmail.com"><HiMail size={24} /></a>
+							</div>
+						</div>
+						<Image src={me} alt="Ernst Legaspi" width={200} height={200} />
+					</div>
+					<div className="projects">
+						<h2>Recent Projects</h2>
+						<div className="p-row">
+							<div onClick={() => {}} className="project">
+							{/* <div onClick={() => setShowTwitterClone(true)} className="project"> */}
+								<Image className="project-image" alt="Twitter Clone Project" src={twitterProject} />
+								<div className="overlay">
+									<p>View project</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="stacks">
+						<h2>Stacks I know</h2>
+						<p className="s-sub-title">Programming Languages / Databases / Others</p>
+						<div className="s-row">
+							<div className="stack">
+								<Image src={reactjs} width={20} height={20} />
+								<p>React.js</p>
+							</div>
+							<div className="stack">
+								<Image src={nodejs} width={25} height={25} />
+								<p>Node.js</p>
+							</div>
+							<div className="stack">
+								<Image src={express} width={20} height={20} />
+								<p>Express.js</p>
+							</div>
+							<div className="stack">
+								<Image src={nextjs} width={20} height={20} />
+								<p>Next.js</p>
+							</div>
+						</div>
+						<div className="s-row">
+							<div className="stack">
+								<Image src={mongodb} width={20} height={20} />
+								<p>MongoDB</p>
+							</div>
+							<div className="stack">
+								<Image src={mysql} width={40} height={20} />
+								<p>MySQL</p>
+							</div>
+							<div className="stack">
+								<Image src={python} width={15} height={15} />
+								<p>Python</p>
+							</div>
+							<div className="stack">
+								<Image src={django} width={15} height={15} />
+								<p>Django</p>
+							</div>
+						</div>
+						<div className="s-row">
+							<div className="stack">
+								<Image src={reactquery} width={15} height={15} />
+								<p>React Query</p>
+							</div>
+							<div className="stack">
+								<Image src={zustand} width={50} height={20} />
+								<p>Zustand</p>
+							</div>
+							<div className="stack">
+								<Image src={axios} width={20} height={20} />
+								<p>Axios</p>
+							</div>
+							<div className="stack">
+								<Image src={redux} width={20} height={20} />
+								<p>Redux</p>
+							</div>
+						</div>
+						<div className="s-row">
+							<div className="stack">
+								<Image src={html} width={15} height={15} />
+								<p>HTML</p>
+							</div>
+							<div className="stack">
+								<Image src={css} width={20} height={20} />
+								<p>CSS</p>
+							</div>
+							<div className="stack">
+								<Image src={tailwind} width={30} height={15} />
+								<p>TailwindCSS</p>
+							</div>
+							<div className="stack">
+								<Image src={typescript} width={15} height={15} />
+								<p>Typescript</p>
+							</div>
+						</div>
+						<div className="s-row">
+							<div className="stack">
+								<Image src={graphql} width={15} height={15} />
+								<p>GraphQL</p>
+							</div>
+							<div className="stack">
+								<Image src={prisma} width={20} height={20} />
+								<p>Prisma</p>
+							</div>
+							<div className="stack">
+								<Image src={mongoose} width={50} height={20} />
+								<p>Mongoose</p>
+							</div>
+							<div className="stack">
+								<Image src={git} width={15} height={15} />
+								<p>Git</p>
+							</div>
+						</div>
+					</div>
+					<div className="education">
+						<h2>Education</h2>
+						<div className="flex justify-center items-center text-center max-[850px]:flex-col">
+							{educationCard(tup, "Technological University of the Philippines", "Bachelor of Science in Computer Science", "2019 - 2023")}
+							{educationCard(dict, "Department of Information and Communications Technology – Philippines", "Software Engineering", "2018 - 2018")}
+							{educationCard(AMA, "AMA Computer College", "Computer Programming", "2016 - 2018")}
+						</div>
+					</div>
+				</div>
+				{/* <Suspense fallback={<p>Loading...</p>}>
+					{showTwitterClone ? <TwitterClone showTwitterClone={setShowTwitterClone} /> : null}
+				</Suspense> */}
+			</div>
+		</div>
+	)
 }
